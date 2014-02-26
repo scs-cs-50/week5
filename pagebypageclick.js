70aa20de614d7796aa71c3a1dbb7aba4f9690478
Timothy Torgenrud <torg@stanford.edu>
2014-02-25 17:56:05 -0800

    Added "main" div instead of data-role="content" showing click event bubble :

---
+++ pagebypageclick.js
@@ -0,0 +1,54 @@
+var counter;
+var bcount =0;
+
+$('#bigbutton').on('click', function() {
+	bcount++;
+	console.log('main code - button click!');
+	console.log('bcount is: " + bcount);
+	   });
+
+
+
+$(document).on('pagecreate','#first_page', function() {
+//     alert('Pagecreate fired');
+});
+
+$(document).on('pageinit','#first_page',function() {
+	$('#bigbutton').on('click', function() {
+		bcount++;
+		console.log('pageinit - button click!');
+		console.log('bcount is: " + bcount);
+	    });
+
+     counter = 1;
+	$('#pageoneanchor').on('click',function(e)
+	{
+	    //		alert("Hopped back to page one!");
+	    console.dir(e);
+	});
+
+});
+
+$(document).on('pagebeforeshow','#first_page',function(event, ui)
+{
+        
+	if (counter >1){
+		$('#pageonetextline').append(' and forth');
+	}
+});
+$(document).on('pageshow','#first_page',function(event, ui)
+{
+	alert('Pageshow fired and counter is ' + counter);
+	counter = counter + 1;
+});
+$(document).on('pagebeforehide','#first_page',function(event, ui)
+{
+//	alert('Pagebeforehide fired');
+});
+$(document).on('pagehide','#first_page',function(event, ui)
+{
+	$('#pageonetextline').append(' and back');
+});
+$(document).on('pagehide','#second_page',function(event, ui)
+{
+});
